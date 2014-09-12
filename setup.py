8d47a58aab3be94b56b55d79f98165b11b72ea02
zeekay <zk@monoid.io>
2014-09-12 04:14:42 -0500

    Update setup.py, gitignore.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Decorum',
-    version='0.4.1',
+    version='0.4.2',
     author='Zach Kelling',
     author_email='zk@monoid.io',
     packages=['decorum',],
