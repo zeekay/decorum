d4a247f462179c470b7f9b8c4532c72a2b3c6ba4
Benoît Bryon <benoit@marmelune.net>
2015-08-01 14:58:56 +0200

    Refs #14 - Make sure there are doctests in README, and they pass.

--- tests/doctests.py
+++ tests/doctests.py
@@ -1,10 +1,8 @@
-import os
-
 import doctest
 
-here = os.path.dirname(os.path.abspath(__file__))
-project_dir = os.path.dirname(here)
 
-
-def test_doctests():
-    doctest.testfile('../README.rst')
+def test_readme():
+    """README contains doctests, they all pass."""
+    result = doctest.testfile('../README.rst')
+    assert result.attempted != 0
+    assert result.failed == 0
