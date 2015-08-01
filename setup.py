f073c61a9bfcda5e929a174c3d812cd02eb4613e
Benoît Bryon <benoit@marmelune.net>
2015-08-01 11:06:17 +0200

    Added 'url' to setup script. Reviewed setup script and manifest.

--- setup.py
+++ setup.py
@@ -1,22 +1,63 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+import os
+import sys
 
-setup(
-    name='Decorum',
-    version='0.5.1',
-    author='Zach Kelling',
-    author_email='zk@monoid.io',
-    packages=['decorum'],
-    license='MIT',
-    description='Tool for writing simple decorators',
-    long_description=open('README.rst').read(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
+from setuptools import setup
 
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
 
-        'License :: OSI Approved :: MIT License',
-    ],
-    keywords='decorator decorators',
-)
+#: Absolute path to directory containing setup.py file.
+here = os.path.abspath(os.path.dirname(__file__))
+#: Boolean, ``True`` if environment is running Python version 2.
+IS_PYTHON2 = sys.version_info[0] == 2
+
+
+# Data for use in setup.
+NAME = 'Decorum'
+DESCRIPTION = 'Tool for writing simple decorators.'
+README = open(os.path.join(here, 'README.rst')).read()
+VERSION = '0.5.1'
+AUTHOR = u'Zach Kelling'
+EMAIL = 'zk@monoid.io'
+LICENSE = 'MIT'
+URL = 'https://pypi.python.org/pypi/{name}'.format(name=NAME)
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+]
+KEYWORDS = [
+    'decorator',
+    'decorators',
+]
+PACKAGES = ['decorum']
+REQUIREMENTS = []
+SETUP_REQUIREMENTS = [
+    'setuptools',
+]
+ENTRY_POINTS = {}
+
+
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(
+        author=AUTHOR,
+        author_email=EMAIL,
+        classifiers=CLASSIFIERS,
+        description=DESCRIPTION,
+        entry_points=ENTRY_POINTS,
+        include_package_data=True,
+        install_requires=REQUIREMENTS,
+        keywords=' '.join(KEYWORDS),
+        license=LICENSE,
+        long_description=README,
+        name=NAME,
+        packages=PACKAGES,
+        setup_requires=SETUP_REQUIREMENTS,
+        url=URL,
+        version=VERSION,
+        zip_safe=False,
+    )
